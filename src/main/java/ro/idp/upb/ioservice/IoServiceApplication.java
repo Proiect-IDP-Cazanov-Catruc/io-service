2eb5eda39505d929003fee30eb47fbb0d66c356b
Ionel Catruc <phonedarkov@gmail.com>
2024-04-14 21:57:44 +0300

    Renamed main class

--- src/main/java/ro/idp/upb/ioservice/IoServiceApplication.java
+++ src/main/java/ro/idp/upb/ioservice/IoServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class IoserviceApplication {
+public class IoServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IoserviceApplication.class, args);
+		SpringApplication.run(IoServiceApplication.class, args);
 	}
 }
--- src/test/java/ro/idp/upb/ioservice/IoServiceApplicationTests.java
+++ src/test/java/ro/idp/upb/ioservice/IoServiceApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IoserviceApplicationTests {
+class IoServiceApplicationTests {
 
 	@Test
 	void contextLoads() {}
